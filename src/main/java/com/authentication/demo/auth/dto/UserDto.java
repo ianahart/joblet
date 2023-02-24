a15d36ddc19ecc6e1c090c184fba220404f6c708
Ian Hart <ianhart7790@gmail.com>
2023-02-24 17:31:46 -0500

    feat: add id to login response

--- src/main/java/com/authentication/demo/auth/dto/UserDto.java
+++ src/main/java/com/authentication/demo/auth/dto/UserDto.java
@@ -3,16 +3,22 @@ package com.authentication.demo.auth.dto;
 import com.authentication.demo.user.Role;
 
 public class UserDto {
+    private Long id;
     private String email;
     private String firstName;
     private String lastName;
     private Role role;
 
-    public UserDto(String email, String firstName, String lastName, Role role) {
+    public UserDto(Long id, String email, String firstName, String lastName, Role role) {
+        this.id = id;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.role = role;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Role getRole() {
@@ -37,6 +43,10 @@ public class UserDto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setFirstName(String firstName) {
