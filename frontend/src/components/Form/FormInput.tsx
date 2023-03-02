da6b36c69dfcb6b27f15095fb9145f7f8780c306
Ian Hart <ianhart7790@gmail.com>
2023-03-02 16:31:29 -0500

    feat: add password visibility toggle on register form

--- frontend/src/components/Form/FormInput.tsx
+++ frontend/src/components/Form/FormInput.tsx
@@ -1,4 +1,5 @@
-import { FormControl, FormLabel, Input, Text } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel, Input, Text } from '@chakra-ui/react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 
 interface IFormInputProps {
   updateField: (name: string, value: string, attribute: string) => void;
@@ -27,6 +28,13 @@ const FormInput = ({
     updateField(name, value, 'value');
   };
 
+  const handleOnClick = () => {
+    console.log(type);
+    type === 'password'
+      ? updateField('password', 'text', 'type')
+      : updateField('password', 'password', 'type');
+  };
+
   const handleOnBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
     if (!validateField(value)) {
@@ -49,17 +57,33 @@ const FormInput = ({
 
   return (
     <FormControl my="1.5rem" display="flex" mx="1rem" flexDir="column">
-      <Input
-        color="text.primary"
-        onChange={handleOnChange}
-        onBlur={handleOnBlur}
-        onFocus={handleOnFocus}
-        width={width}
-        name={name}
-        id={name}
-        type={type}
-        value={value}
-      />
+      <Box position="relative">
+        <Input
+          color="text.primary"
+          onChange={handleOnChange}
+          onBlur={handleOnBlur}
+          onFocus={handleOnFocus}
+          width={width}
+          name={name}
+          id={name}
+          type={type}
+          value={value}
+        />
+      </Box>
+      {name === 'password' && (
+        <Box
+          zIndex="5"
+          onClick={handleOnClick}
+          cursor="pointer"
+          position="absolute"
+          color="text.primary"
+          top="10px"
+          fontSize="1.5rem"
+          right="40px"
+        >
+          {type === 'password' ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+        </Box>
+      )}
       <FormLabel color="text.primary" width={width} htmlFor={htmlFor}>
         {label}
       </FormLabel>
