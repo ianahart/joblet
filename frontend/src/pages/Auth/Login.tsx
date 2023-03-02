9b6ece16174fb02cae3c7e2e06362360cb04fe51
Ian Hart <ianhart7790@gmail.com>
2023-03-01 19:56:24 -0500

    feat: add background images to register and login pages

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -1,7 +1,17 @@
 import { Box } from '@chakra-ui/react';
+import loginBG from '../../images/login.png';
 
 const Login = () => {
-  return <Box>Login page</Box>;
+  return (
+    <Box
+      minH="100vh"
+      backgroundImage={`url(${loginBG})`}
+      backgroundPosition="center"
+      backgroundSize="cover"
+    >
+      Login page
+    </Box>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -1,7 +1,17 @@
 import { Box } from '@chakra-ui/react';
+import registerBG from '../../images/register.png';
 
 const Register = () => {
-  return <Box>Register page</Box>;
+  return (
+    <Box
+      minH="100vh"
+      backgroundImage={`url(${registerBG})`}
+      backgroundPosition="center"
+      backgroundSize="cover"
+    >
+      Register page
+    </Box>
+  );
 };
 
 export default Register;
