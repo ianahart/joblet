54373ba27eb97ac5f98935bb47d36eb70ebe19e0
Ian Hart <ianhart7790@gmail.com>
2023-04-05 14:04:20 -0400

    deployment: change axios configuration

--- frontend/src/helpers/utils.ts
+++ frontend/src/helpers/utils.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const http = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/',
+  baseURL: 'https://prod-joblet.netlify.app/api/v1/',
 });
 
 export const retreiveTokens = () => {
